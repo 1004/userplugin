d00dfdb461dbfaa832a6e725a0e78948a495f39b
DESKTOP-K2G4F32\86137 <xiekongying001@ke.com>
2019-10-27 16:56:55 +0800

    feature:用户插件接入成功

---
+++ app/src/main/java/com/aike/plugin/user/main/UserMainActivity.java
@@ -0,0 +1,18 @@
+package com.aike.plugin.user.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.aike.plugin.user.R;
+
+/**
+ * @Author xiekongying001
+ * @Date 2019/10/27 16:08
+ * @Des
+ */
+public class UserMainActivity extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
