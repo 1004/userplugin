ed25cc76bbe4195e806a81d7a4586fe0a754b0a4
谢孔营 <xiekongying001@ke.com>
2019-11-15 18:07:32 +0800

    feature:user插件进入Router

--- app/src/main/java/com/aike/plugin/user/main/UserMainActivity.java
+++ app/src/main/java/com/aike/plugin/user/main/UserMainActivity.java
@@ -2,17 +2,27 @@ package com.aike.plugin.user.main;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import com.aike.plugin.user.R;
+import com.aike.router.Route;
+import com.aike.router.Router;
 
 /**
  * @Author xiekongying001
  * @Date 2019/10/27 16:08
  * @Des
  */
+@Route("xky://user/main")
 public class UserMainActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Router.create("xky://course/main").navigate(UserMainActivity.this);
+      }
+    });
   }
 }
